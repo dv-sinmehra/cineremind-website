845958c24e9e4a2ba40a5e153cc73014b387116c
dv-sinmehra <divijsinghmehra456@gmail.com>
2026-07-13 13:57:38 +0530

    init repo with html and css shell

---
+++ main.js
@@ -0,0 +1,3 @@
+const PLACEHOLDER_IMAGE = 'data:image/svg+xml,' + encodeURIComponent(
+  '<svg xmlns="http://www.w3.org/2000/svg" width="300" height="450" fill="%231a1a26"><rect width="300" height="450"/><text x="50%" y="50%" text-anchor="middle" fill="%23475569" font-size="14">No Image</text></svg>'
+);
